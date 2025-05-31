fa29468a261fa11b25e4a7b35776d4c1b5733573
HardBoss07 <matteo@bosshard.net>
2025-05-31 18:06:00 +0200

    fixed quick sort

--- sorting-algorythms/quick-sort/src/main.rs
+++ sorting-algorythms/quick-sort/src/main.rs
@@ -2,7 +2,7 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 fn main() {
-    let length = 50;
+    let length = 1000;
 
     let mut list: Vec<u32> = (1..=length).collect();
 
@@ -14,22 +14,21 @@ fn main() {
     println!("=========================================");
 
     println!("Unsorted list:\n{:?}", list);
-
     println!("-----------------------------------------");
 
-    quick_sort(&mut list, 0, list.len() as isize - 1);
+    let len = list.len();
+    quick_sort(&mut list, 0, len as isize - 1);
 
     println!("-----------------------------------------");
-
     println!("Sorted list:\n{:?}", list);
 }
 
 fn quick_sort(list: &mut Vec<u32>, low: isize, high: isize) {
     if low < high {
-        let p = partition(list, low, high);
+        let pivot_index = partition(list, low, high);
         println!("Current list:\n{:?}", list);
-        quick_sort(list, low, p - 1);
-        quick_sort(list, p + 1, high);
+        quick_sort(list, low, pivot_index - 1);
+        quick_sort(list, pivot_index + 1, high);
     }
 }
 
