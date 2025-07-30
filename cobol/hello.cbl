61daefcadac052bd4f240f5ee82ef541734a01bb
HardBoss07 <matteo@bosshard.net>
2025-07-30 19:55:30 +0200

    tried out some cobol

---
+++ cobol/hello.cbl
@@ -0,0 +1,9 @@
+IDENTIFICATION DIVISION.
+    PROGRAM-ID. HelloWorld.
+
+    ENVIRONMENT DIVISION.
+
+    DATA DIVISION.
+        PROCEDURE DIVISION.
+        DISPLAY "Hello, World!".
+        STOP RUN.
